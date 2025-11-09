c2fedd76369a30e4d9a0adfed686504a51a62359
affandhanbad-dev <affandhanbad@gmail.com>
2025-11-09 14:42:19 +0530

    adding port

--- Webapp-Docker-code/app.py
+++ Webapp-Docker-code/app.py
@@ -58,4 +58,4 @@ def upload_file():
                                confidence_score=confidence_score)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host = '0.0.0.0', port=5000, debug=True)
